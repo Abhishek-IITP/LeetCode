2c8ec45ca4d4cf9e22e4995630f1d190c1db8170
Abhishek Kumar Mohanty <165031056+Abhishek-IITP@users.noreply.github.com>
2025-03-22 16:15:14 +0530

    new commit

---
+++ 724.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int rightSum=0;
+        int n= nums.size();
+
+        for(int i=0;i<n;i++){
+            rightSum+= nums[i];
+        }
+
+        int leftSum=0;
+
+        for(int i=0; i<n;i++){
+            if(leftSum == rightSum-nums[i]){
+                return i;
+            }
+            leftSum += nums[i];
+            rightSum -= nums[i];
+
+        }
+        return -1;
+    }
+};
